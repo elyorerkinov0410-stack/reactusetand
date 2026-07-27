599f4a629f298e57442c2b713f176f4da26ab9a0
Elyor Erkinov <elyorerkinov0410@gmail.com>
2026-07-27 08:57:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Card1 from "./components/CARD/Card1";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Card1 />
+    </div>
+  );
+}
